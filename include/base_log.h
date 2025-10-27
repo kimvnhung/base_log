2abc63878ff186c283c7e2badea5476185022077
Kim Văn Hùng <hungkv1@viettel.com.vn>
2025-10-27 11:38:25 +0700

    update logD and fix build with shared lib

--- include/base_log.h
+++ include/base_log.h
@@ -9,25 +9,36 @@
 #define LOG_TAG "BASE_LOG"
 #endif
 
-#define FUNCTION_NAME fmt::format("{} {}", LOG_TAG, __func__).c_str()
+#ifndef PROJECT_SOURCE_DIR
+#define PROJECT_SOURCE_DIR ""
+#endif
 
-#define TRACE(...)          log_message("TRACE",FUNCTION_NAME, "{}", __VA_ARGS__)
-#define TRACEF(format,...)  log_message("TRACE",FUNCTION_NAME,format, __VA_ARGS__)
+#define FILE_PATH_REMOVE_PREFIX(path, prefix) \
+    (std::strncmp(path, prefix, sizeof(prefix) - 1) == 0 \
+        ? (path + sizeof(prefix) - 1) \
+        : path)
 
-#define DEBUG(...)          log_message("DEBUG",FUNCTION_NAME, "{}", __VA_ARGS__)
-#define DEBUGF(format,...)  log_message("DEBUG",FUNCTION_NAME,format, __VA_ARGS__)
+#define FILE_NAME FILE_PATH_REMOVE_PREFIX(__FILE__, PROJECT_SOURCE_DIR)
 
-#define INFO(...)          log_message("INFO",LOG_TAG, "{}", __VA_ARGS__)
-#define INFOF(format,...)  log_message("INFO",LOG_TAG,format, __VA_ARGS__)
+#define FUNCTION_NAME fmt::format("{} {} {}", LOG_TAG, FILE_NAME,__LINE__).c_str()
 
-#define WARN(...)          log_message("WARN",LOG_TAG, "{}", __VA_ARGS__)
-#define WARNF(format,...)  log_message("WARN",LOG_TAG,format, __VA_ARGS__)
+#define TRACE(...)          log_message("T",FUNCTION_NAME, "{}", __VA_ARGS__)
+#define TRACEF(format,...)  log_message("T",FUNCTION_NAME,format, __VA_ARGS__)
 
-#define ERROR(...)          log_message("ERROR",LOG_TAG, "{}", __VA_ARGS__)
-#define ERRORF(format,...)  log_message("ERROR",LOG_TAG,format, __VA_ARGS__)
+#define DEBUG(...)          log_message("D",FUNCTION_NAME, "{}", __VA_ARGS__)
+#define DEBUGF(format,...)  log_message("D",FUNCTION_NAME,format, __VA_ARGS__)
 
-#define CRITICAL(...)          log_message("CRITICAL",LOG_TAG, "{}", __VA_ARGS__)
-#define CRITICALF(format,...)  log_message("CRITICAL",LOG_TAG,format, __VA_ARGS__)
+#define INFO(...)          log_message("I",FUNCTION_NAME, "{}", __VA_ARGS__)
+#define INFOF(format,...)  log_message("I",FUNCTION_NAME,format, __VA_ARGS__)
+
+#define WARN(...)          log_message("W",FUNCTION_NAME, "{}", __VA_ARGS__)
+#define WARNF(format,...)  log_message("W",FUNCTION_NAME,format, __VA_ARGS__)
+
+#define ERROR(...)          log_message("E",FUNCTION_NAME, "{}", __VA_ARGS__)
+#define ERRORF(format,...)  log_message("E",FUNCTION_NAME,format, __VA_ARGS__)
+
+#define CRITICAL(...)          log_message("C",FUNCTION_NAME, "{}", __VA_ARGS__)
+#define CRITICALF(format,...)  log_message("C",FUNCTION_NAME,format, __VA_ARGS__)
 
 
 /// Only forward declaration of dispatcher (defined in .cpp)
@@ -39,3 +50,20 @@ void log_message(const char* type, const char* tag, const char* format, Args&&..
     log_dispatch(type,tag,formatted);
 }
 
+#define LOGT(...)           TRACE(__VA_ARGS__)
+#define LOGTF(...)          TRACEF(__VA_ARGS__)
+
+#define LOGD(...)           DEBUG(__VA_ARGS__)
+#define LOGDF(...)          DEBUGF(__VA_ARGS__)
+
+#define LOGI(...)           INFO(__VA_ARGS__)
+#define LOGIF(...)          INFOF(__VA_ARGS__)
+
+#define LOGW(...)           WARN(__VA_ARGS__)
+#define LOGWF(...)          WARNF(__VA_ARGS__)
+
+#define LOGE(...)           ERROR(__VA_ARGS__)
+#define LOGEF(...)          ERRORF(__VA_ARGS__)
+
+#define LOGC(...)           CRITICAL(__VA_ARGS__)
+#define LOGCF(...)          CRITICALF(__VA_ARGS__)
--- src/base_log.cpp
+++ src/base_log.cpp
@@ -1,8 +1,18 @@
 #include <base_log.h>
 
-#include <cstring>
+#include <chrono>
+#include <ctime>
+#include <iomanip>
 #include <iostream>
 
-void log_dispatch(const char* type,const char* tag, const std::string& message) {
-    std::cout << "[" << type << "] [" << tag << "] " << message << std::endl;
+void log_dispatch(const char* type, const char* tag, const std::string& message) {
+    auto t = std::chrono::system_clock::now();
+    auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t.time_since_epoch()) % 1000;
+
+    std::time_t tt = std::chrono::system_clock::to_time_t(t);
+    std::tm tm = *std::localtime(&tt);
+
+    std::cout << "[" << std::put_time(&tm, "%Y-%m-%d %H:%M:%S") << ":"
+              << std::setfill('0') << std::setw(3) << ms.count() << "] "
+              << "[" << type << "] [" << tag << "]: " << message << std::endl;
 }
